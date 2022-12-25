9f0e81b4c5f9f24eb519c7911f4f4e51e3981020
吉士 <57695239+heaid@users.noreply.github.com>
2022-12-25 12:58:56 +0800

    Update warp_change_ip.sh

--- warp_change_ip.sh
+++ warp_change_ip.sh
@@ -6,7 +6,7 @@
 UA_Browser="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36"
 read -r -p "Is warp installed? [y/n] " input
 if [[ "$input" == "n" ]];then
-    bash <(curl -fsSL https://github.com/heaid/warp.sh/raw/main/warp.sh) 4
+    bash <(curl -fsSL https://github.com/luoxue-bot/warp.sh/raw/main/warp.sh) 4
 elif [[ "$input" == "y" ]];then
     read -r -p "Input the region you want(e.g. HK,SG):" area
 fi
